9f550f3e83515b8cbd3c5283563c3abc88f48013
Florian Henkel <florian.henkel@jku.at>
2021-11-02 13:11:20 +0100

    update model

--- page_turner/config.py
+++ page_turner/config.py
@@ -4,8 +4,9 @@ SCORE_HEIGHT = 1181
 SCORE_WIDTH = 835
 SCALE_WIDTH = 416
 SCALE_FACTOR = SCORE_HEIGHT / SCALE_WIDTH
+SPEC_VIS_WINDOW = 400
 PADDING = (SCORE_HEIGHT - SCORE_WIDTH) // 2
 
-DEFAULT_MODEL = os.path.join('..', 'models', 'model', 'best_model.pt')
+DEFAULT_MODEL = os.path.join('..', 'models', 'test_model', 'best_model.pt')
 DEFAULT_DIR = os.path.join('..', 'demo_piece')
 
